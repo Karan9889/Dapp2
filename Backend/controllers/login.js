49fc21390a0ad94dc83e65c104af540d0a5e4089
Karan9889 <43220806+Karan9889@users.noreply.github.com>
2020-04-07 17:27:03 -0400

    Private and Secret and Toekn added

---
+++ Backend/controllers/login.js
@@ -0,0 +1,16 @@
+var user = require('../models/user');
+var passport = require('passport');
+
+exports.login = (req, res) => {
+    passport.authenticate('local', (err, user, info) => {
+        var token;
+
+        if(err){
+            console.log(err);
+            res.status(404);
+        }else{
+            
+        }
+    })
+}
+© 2020 GitHub,
