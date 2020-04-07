03e964ed0b918befcc0cf9b1b3c1fddd2385513b
Karan9889 <43220806+Karan9889@users.noreply.github.com>
2020-04-07 17:43:26 -0400

    Route Changes

---
+++ Backend/models/car.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const carSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'user'
+    },
+    make: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    seatCapacity: {
+        type: Number,
+        required: true
+    },
+    licencePlateNum: {
+        type: String,
+        required: true
+    },
+    luggageCapacity: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Car', carSchema);
