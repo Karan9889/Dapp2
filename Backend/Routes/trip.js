b1e13c0e393eafac9f068b33433b0bf2028a92ef
Karan9889 <43220806+Karan9889@users.noreply.github.com>
2020-04-07 18:00:34 -0400

    Getting Trip Requests for a trip

---
+++ Backend/Routes/trip.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const trip = require('../controllers/trip');
+
+router.post('/create', trip.createTrip)
+router.post('/createTripRequest', trip.createTripRequest)
+router.get('/find/tripRequest/:tripId', trip.getTripRequestsForTrip)
+
+module.exports = router;
