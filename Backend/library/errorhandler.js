8358ef9bef0a29c40e2413a2abe6fbf6777a3d3e
Karan9889 <43220806+Karan9889@users.noreply.github.com>
2020-04-07 17:37:09 -0400

    edited place and trip modules

---
+++ Backend/library/errorhandler.js
@@ -0,0 +1,73 @@
+const Joi=require("joi")
+//place config
+let joiConfigPlace=function(reqbody)
+{
+    
+    const joiSchema=Joi.object().keys({
+        
+        name:Joi.string().required(),
+        navPointType:Joi.string().required(),
+        location:Joi.required()
+    })
+    let joiRes=Joi.validate(reqbody,joiSchema,{abortEarly:false})
+    return joiRes;
+}
+
+//place errors functionality
+let placeErrors=function(errorsdetails)
+{
+    errorArr=[]
+
+    errorsdetails.forEach(function(detail) {
+        errorArr.push({
+            key: detail.path,
+            message: detail.message
+        });
+    });
+    return errorArr
+
+}
+
+//trip errors
+let joiConfigTrip=function(reqBody)
+{
+    // ["from", "to","cost","departureTime","arivalTime","driver",
+    // "passengers","rating","car"]
+    const joiSchema=Joi.object().keys({
+        from:Joi.required(),
+        to:Joi.required(),
+        cost:Joi.number().strict().required(),
+        departureTime:Joi.date().iso(),
+        arivalTime:Joi.date().iso().min(Joi.ref('departureTime')),
+        seatsAvailable:Joi.number().required(),
+        luggage:Joi.string().required(),
+        driver:Joi.required(),
+        passengers:Joi.array().required(),
+        rating:Joi.number().strict().required(),
+        car:Joi.required()
+
+        
+    })
+    let joiResult=Joi.validate(reqBody,joiSchema,{abortEarly:false})
+    return joiResult
+
+}
+let tripErrors=function(errorsdetails)
+{
+    errorArr=[]
+    errorsdetails.forEach(function(detail) {
+        errorArr.push({
+            key: detail.path,
+            message: detail.message
+        });
+    });
+    return errorArr
+
+}
+
+module.exports={
+    joiConfigPlace,
+    placeErrors,
+    joiConfigTrip,
+    tripErrors
+}
