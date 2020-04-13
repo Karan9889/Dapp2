663d65689db63e557418466a68a5ac4809ecd43c
Karan9889 <43220806+Karan9889@users.noreply.github.com>
2020-04-13 15:06:25 -0400

    stuff

---
+++ Frontend/src/app/models/User.ts
@@ -0,0 +1,11 @@
+export class User {
+    firstname: String;
+    lastname: String;
+    email: String;
+    password: String;
+    country: String;
+    province: String;
+    city: String;
+    phone: Number;
+    terms: Boolean;
+}
